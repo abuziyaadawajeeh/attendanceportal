853820a52aa00fa5e36fb9157b585b0750cc81f9
Shakkir Moulana <41133926+shakkirmln@users.noreply.github.com>
2020-06-20 16:22:44 +0530

    Create loginstudent.php

---
+++ php/loginstudent.php
@@ -0,0 +1,54 @@
+<?php
+
+$rollno=$_POST["rollno"];
+$password=$_POST["password"];
+$query = "select * from student where rollno =?;";
+include ("conn.php");
+if($conn->error)
+{
+    header("Location:../index.php?dbconnerror");
+    exit();
+}
+$stmt=mysqli_stmt_init($conn);
+if(!mysqli_stmt_prepare($stmt, $query))
+    header("location:../index.php?stmtpreparationfailed");
+mysqli_stmt_bind_param($stmt, "s",$rollno);
+mysqli_stmt_execute($stmt);
+// mysqli_stmt_store_result($stmt);
+// $num=mysqli_stmt_num_rows($stmt);
+// if($num==0)
+//     header("Location:../index.php?registerfirst");
+$result=mysqli_stmt_get_result($stmt);
+$row=mysqli_fetch_assoc($result);
+mysqli_stmt_close($stmt);
+if(!$row){
+    header("Location:../index.php?message=registerfirst");
+
+    exit();
+
+}
+
+else
+{
+    if(!password_verify($password,$row["pwd"])){
+        header("Location:../index.php?message=incorrectpassword");
+    
+        exit();
+
+    }
+    else{
+        session_start();
+        $_SESSION["rollno"]=$row["rollno"];
+        $_SESSION["name"]=$row["name"];
+
+        there: 
+        include ("conn.php");
+
+  
+        header("location:../main.php?loginsuccessful?".$_SESSION["name"]");
+        exit();
+        }
+        
+        exit();
+    }
+}
