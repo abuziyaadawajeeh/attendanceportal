b17e3e1afa4aa79cef30b12217a33e1c2f18559c
abuziyaada <mswaji09@gmail.com>
2020-06-20 14:27:20 +0530

    student dashboard initialised

---
+++ student.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/student.css">
+</head>
+<body>
+    <div class="item">
+        <span class="cname">coursename</span><span class="fname">facultyname</span>
+    </div>
+    <div class="dates">
+        dates absent
+    </div>
+    <div class="item">
+        <span class="cname">coursename</span><span class="fname">facultyname</span>
+    </div>
+    <div class="dates">
+        dates absent
+    </div>
+    <div class="item">
+        <span class="cname">coursename</span><span class="fname">facultyname</span>
+    </div>
+    <div class="dates">
+        dates absent
+    </div>
+    <div class="item">
+        <span class="cname">coursdfsdfdsfsdfdsfsdename</span><span class="fname">facultyname</span>
+    </div>
+    <div class="dates">
+        dates absent
+    </div>
+    <div class="item">
+        <span class="cname">coursename</span><span class="fname">facultsdfdsfsdfdsyname</span>
+    </div>
+    <div class="dates">
+        dates absent
+    </div>
+</body>
+</html>
