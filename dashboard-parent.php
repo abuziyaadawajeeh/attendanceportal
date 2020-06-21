78aa4fb039f8d17512667c6d7a2cc02fb69eef28
Shakkir Moulana <41133926+shakkirmln@users.noreply.github.com>
2020-06-21 12:23:43 +0530

    Add files via upload

---
+++ dashboard-parent.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+if(!isset($_SESSION["uname"]) )
+  header("Location:../login-parent.php?loginproperly");
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v4.0.1">
+    <title>Cover Template · Bootstrap</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+    <!-- Bootstrap core CSS -->
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+      body{
+        background-image: linear-gradient(to right top, #58bfe0, #61abcb, #6597b5, #66849d, #627285, #627787, #647b89, #66808b, #719da0, #87b9ab, #acd3b0, #deeab5);
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+
+  </head>
+  <body class="text-center">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="masthead mb-auto">
+    <div class="inner">
+      <h3 class="masthead-brand">ATTENDANCE PORTAL </h3>
+      <nav class="nav nav-masthead justify-content-center">
+        <a class="nav-link active" href="#">Home</a>
+        <a class="nav-link active" href="#">View Attendance</a>&nbsp
+	<a class="nav-link active" href="#">Query</a>&nbsp
+	<a class="nav-link active" href="./php/logout.php">Logout</a>&nbsp
+      </nav>
+    </div>
+  </header>
+
+  <main role="main" class="inner cover">
+    <h1 class="cover-heading">Hi 
+	<?php 
+	echo $_SESSION["uname"]; 
+	 ?></h1>
+  </main>
+
+  <footer class="mastfoot mt-auto">
+    <div class="inner">
+     <p>IIITDM</p>
+    </div>
+  </footer>
+</div>
+</body>
+</html>
---
+++ signup-student.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign Up Form by Colorlib</title>
+
+
+    <link rel="stylesheet" href="fontss/material-icon/css/material-design-iconic-font.min.css">
+
+    
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <div class="main">
+
+        <section class="signup">
+            <!-- <img src="images/signup-bg.jpg" alt=""> -->
+            <div class="container">
+                <div class="signup-content">
+                    <form method="POST" action="./php/registerstudent.php" id="signup-form" class="signup-form">
+                        <h2 class="form-title">Create account</h2>
+                        <div class="form-group">
+                            <input type="text" class="form-input" name="name" id="name" placeholder="Your Name"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-input" name="roll" id="roll" placeholder="Your Roll NO"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-input" name="department-id" id="roll" placeholder="Department-id"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-input" name="semester" id="roll" placeholder="semester-No"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-input" name="password" id="password" placeholder="Password"/>
+                           
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-input" name="re_password" id="re_password" placeholder="Repeat your password"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
+                            <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
+                        </div>
+<div style="display: none" id="message"><?php if(isset($_GET["message"]))
+                                  echo $_GET["message"]; ?>
+          
+          </div> <br>
+                        <div class="form-group">
+                            <input type="submit" name="submit" id="submit" class="form-submit" value="Sign up"/>
+                        </div>
+                    </form>
+                    <p class="loginhere">
+                        Have already an account ? <a href="login-student.php" class="loginhere-link">Login here</a><br><br>
+                        Want to go back? <a href ="index.html" class="loginhere-link">Click here</a> 
+                    </p>
+                </div>
+            </div>
+        </section>
+
+    </div>
+
+    <!-- JS -->
+    
+    <script src="js/mains.js"></script>
+</body>
+</html>
